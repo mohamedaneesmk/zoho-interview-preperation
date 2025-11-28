7990f6cf52a050805674766225d6f7f2ece074f5
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-11-28 17:32:57 +0530

    Added DeleteColumnsToMakeSorted.java Solution

--- Round-2/DeleteColumnsToMakeSorted.java
+++ Round-2/DeleteColumnsToMakeSorted.java
@@ -8,7 +8,7 @@ public class DeleteColumnsToMakeSorted {
         int rows = strs.length;
         int cols = strs[0].length();
         int deleteCount = 0;
-
+ 
         for (int c = 0; c < cols; c++) {
             for (int r = 1; r < rows; r++) {
                 if (strs[r].charAt(c) < strs[r - 1].charAt(c)) {
