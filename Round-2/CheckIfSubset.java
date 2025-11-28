cdeb117dfb7e322baf243eac1ca5da8f933786af
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-11-28 17:31:11 +0530

    Added CheckIfSubset.java Solution

--- Round-2/CheckIfSubset.java
+++ Round-2/CheckIfSubset.java
@@ -35,7 +35,7 @@ public class CheckIfSubset {
 
     private static boolean isAllElementsPresent(int[] nums1, int[] nums2) {
         int i=0,j=0;
-
+ 
         while (i<nums1.length && j<nums2.length) {
             if(nums1[i]<nums2[j]) i++;
             else if(nums1[i]==nums2[j]){
