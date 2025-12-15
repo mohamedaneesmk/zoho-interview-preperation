5c51849d1f5b32e46fbd02d0acefd208a1f430b3
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-12-15 18:56:13 +0530

    Set Matrix Zero LeetCode #75

--- Round-2/MatrixZeroOptimized.java
+++ Round-2/MatrixZeroOptimized.java
@@ -56,4 +56,21 @@ public class MatrixZeroOptimized {
             }
         }
     }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                { 1, 1, 1 },
+                { 1, 0, 1 },
+                { 1, 0, 1 }
+        };
+
+        setZeroes(matrix);
+
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
 }
