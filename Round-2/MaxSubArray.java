3f2db5de7f1610b2db3d8c1630c721547394245d
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-01-04 11:39:31 +0530

    Print the Sub Array

--- Round-2/MaxSubArray.java
+++ Round-2/MaxSubArray.java
@@ -18,7 +18,7 @@
  
  */
 
-class MaxSubArray {
+/*class MaxSubArray {
     public static void main(String[] args) {
         // int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
         // int[] nums = {1};
@@ -35,6 +35,47 @@ class MaxSubArray {
             currentSum = Math.max(nums[i], currentSum + nums[i]);
             maxSum = Math.max(maxSum, currentSum);
         }
+
+        return maxSum;
+    }
+}*/
+
+// To Print Subarray as well
+public class MaxSubArray {
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        // int[] nums = {1};
+        // int[] nums = {5, 4, -1, 7, 8};
+        int result = findMaxSubArray(nums);
+        System.out.println("Maximum Subarray Sum: " + result);
+    }
+
+    private static int findMaxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+        int start = 0, end = 0, tempStart = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > currentSum + nums[i]) {
+                currentSum = nums[i];
+                tempStart = i;
+            } else {
+                currentSum += nums[i];
+            }
+
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+                start = tempStart;
+                end = i;
+            }
+        }
+
+        System.out.print("Subarray with maximum sum: [");
+        for (int i = start; i <= end; i++) {
+            System.out.print(nums[i]);
+            if (i < end) System.out.print(", ");
+        }
+        System.out.println("]");
 
         return maxSum;
     }
